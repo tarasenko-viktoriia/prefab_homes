baf9432f91fd3a3a5dabff4a4add99a25d65619e
victoriataracenko <victoriataracenko0412@gmail.com>
2025-03-14 11:58:56 +0200

    automatic change of the main image is configured

--- js/script.js
+++ js/script.js
@@ -8,8 +8,6 @@ function windowLoad() {
 	document.addEventListener('click', documentActions);
 	document.addEventListener('keydown', documentActions);
 	html.classList.add('loaded');
-	// slidersInit()
-	// scrollActions()
 }
 function documentActions(e) {
 	const type = e.type
@@ -34,22 +32,46 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 });
 
-const thumbnailSlider = new Swiper(".thumbnail-slider", {
-	direction: "vertical",
-    slidesPerView: "auto",
-    spaceBetween: 10,
-    watchSlidesProgress: true,
-    allowTouchMove: false,
-});
+// const thumbnailSlider = new Swiper(".thumbnail-slider", {
+// 	direction: "vertical",
+//     slidesPerView: "auto",
+//     spaceBetween: 10,
+//     watchSlidesProgress: true,
+//     allowTouchMove: false,
+// });
 
-const mainSlider = new Swiper(".main-slider", {
-    direction: "vertical",
-    loop: true,
-    loopedSlides: 5,
-    navigation: { nextEl: ".button-next", prevEl: ".button-prev" },
-    thumbs: { swiper: thumbnailSlider },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,  
-    },
-});
+// const mainSlider = new Swiper(".main-slider", {
+//     direction: "vertical",
+//     loop: true,
+//     loopedSlides: 5,
+//     navigation: { nextEl: ".button-next", prevEl: ".button-prev" },
+//     thumbs: { swiper: thumbnailSlider },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,  
+//     },
+// });
+
+const images = [
+	"./img/home-section/image-4.png",
+	"./img/banner.jpg",
+	"./img/home-section/image-4.png",
+	"./img/home-section/image-4.png"
+];
+
+let currentIndex = 0;
+
+function changeImage() {
+	const heroImage = document.getElementById("heroImage");
+
+	// heroImage.classList.add('fade-out');
+
+	// setTimeout(() => {
+		currentIndex = (currentIndex + 1) % images.length;
+		heroImage.src = images[currentIndex];
+
+	// 	heroImage.classList.remove('fade-out');
+	// }, 500); 
+}
+
+setInterval(changeImage, 10000);
